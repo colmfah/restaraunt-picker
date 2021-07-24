e4ace5f4810196c075b67c958cb736fb19b3a3c6
colmfah <colmdfahey@gmail.com>
2021-07-24 11:08:00 +0000

    added search functionality

--- restaraunts/views.py
+++ restaraunts/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Restaraunt
 
 # Create your views here.
-
 
 def all_restaraunts(request):
     """ A view to show all restaraunts, including sorting and search queries """
 
     restaraunts = Restaraunt.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('restaraunts'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            restaraunts = restaraunts.filter(queries)
 
     context = {
         'restaraunts': restaraunts,
+        'search_term': query,
     }
 
     return render(request, 'restaraunts/restaraunts.html', context)
