3efe3863ba39b71c3ea7b11c3e60609da589c2e1
colmfah <colmdfahey@gmail.com>
2021-07-24 09:33:37 +0000

    added restaraunt views and templates

--- restaraunts/views.py
+++ restaraunts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Restaraunt
 
 # Create your views here.
+
+
+def all_restaraunts(request):
+    """A view to show all restaraunts, including sorting and search queries """
+
+    restaraunts = Restaraunt.objects.all()
+
+    context = {
+        'restaraunts': restaraunts,
+    }
+
+    return render(request, 'restaraunts/restaraunts.html', context)
