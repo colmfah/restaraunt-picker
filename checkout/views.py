43d8a0296592b69cf5a07da6dd22f58d8737e9a4
colmfah <colmdfahey@gmail.com>
2021-07-25 06:35:59 +0000

    add stripe integration

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_YIQAQ7ERzdexV6HUXw4xOSQk'
     }
 
     return render(request, template, context)
