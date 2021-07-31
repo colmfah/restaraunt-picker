05b09200f6c15290e0e55a67d66c7085a5e3a88a
colmfah <colmdfahey@gmail.com>
2021-07-31 15:24:15 +0000

    deployment changes

--- restaraunt_picker/settings.py
+++ restaraunt_picker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ig=9@d30bf*4*m0p2y($v0x^i1$eemr6#&2$s0xmy2$lg^@9na
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cf-restaraunt-picker.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -114,12 +114,17 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'restaraunt_picker.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
@@ -173,3 +178,4 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'hello@restarauntpicker.com'
+
