a1165b6cbc0a66b5f1082dd51453c12c6a34bb4e
colmfah <colmdfahey@gmail.com>
2021-07-31 15:44:14 +0000

    removed secret key and set debug

--- restaraunt_picker/settings.py
+++ restaraunt_picker/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ig=9@d30bf*4*m0p2y($v0x^i1$eemr6#&2$s0xmy2$lg^@9na'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cf-restaraunt-picker.herokuapp.com', 'localhost']
 
