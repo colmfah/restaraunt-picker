e86a2cc69d2562e812ef041cf85e9f4908095413
colmfah <colmdfahey@gmail.com>
2021-07-31 16:53:41 +0000

    fix bucket name

--- restaraunt_picker/settings.py
+++ restaraunt_picker/settings.py
@@ -169,7 +169,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'cf-restaraunt-picker'
+    AWS_STORAGE_BUCKET_NAME = 'restaraunt-picker'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
